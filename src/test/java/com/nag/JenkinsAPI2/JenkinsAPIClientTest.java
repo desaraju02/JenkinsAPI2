f858b53d627f7d595093ed42acb7e86f07cd8e81
Nages Desaraju <desaraju02@gmail.com>
2017-07-11 19:18:20 +0530

    Adding first set of classes

---
+++ src/test/java/com/nag/JenkinsAPI2/JenkinsAPIClientTest.java
@@ -0,0 +1,14 @@
+package com.nag.JenkinsAPI2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JenkinsAPIClientTest {
+
+	@Test
+	public void testListJobs() {
+		assertEquals("JenkinsAPI", "JenkinsAPI");
+	}
+
+}
